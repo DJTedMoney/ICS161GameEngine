1f75a3c8a2615853a83bf7ab9e49a6a89fc73db2
Hector Martinez <hectoram@uci.edu>
2015-02-20 16:15:39 -0800

    SoundHandler.h skeleton suggestion

---
+++ GameEngineProject/GameEngineProject/SoundHandler.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+
+class SoundHandler 
+{
+
+public:
+	//Might need to have a string as a parameter to know where to look to load sounds
+	SoundHandler();
+
+	~SoundHandler();
+
+	void playSound(std::string toPlay);
+	void setVolume(int volumeLevel);
+	void setLoop(bool loop);
+
+
+};
