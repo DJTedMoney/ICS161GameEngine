215c0d32398e29ec3c248cc42d97a6b05dc4eaca
Raymond Chen <raymoc2@uci.edu>
2015-02-23 20:13:22 -0800

    Updated Actor.h

--- GameEngineProject/GameEngineProject/Actor.h
+++ GameEngineProject/GameEngineProject/Actor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "SpriteHandler.h"
 #include <string>
+#include <map>
 
 class Actor {
 public:
@@ -19,8 +20,15 @@ public:
 	std::string getName();
 	void setName(std::string name);
 
+	// Attributes
+	void addAttribute(std::string key, std::string value);
+	std::string getAttribute(std::string key);
+	void removeAttribute(std::string key);
+
 private:
 	SpriteHandler sprite; 
+
+	std::map<std::string, std::string> attributes; //key-value pairs describing attributes of the actor
 
 	int x;
 	int y;
