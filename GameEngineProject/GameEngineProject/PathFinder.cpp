2d6d17cd12bf165e2729bb1227ebd496b3b513d3
Cory Sherman <coryshrmn@gmail.com>
2015-02-24 17:34:50 -0800

    pathfinder compiles

---
+++ GameEngineProject/GameEngineProject/PathFinder.cpp
@@ -0,0 +1,156 @@
+#include "PathFinder.h"
+
+#include <cstdlib>
+#include <iostream>
+
+template <std::size_t width, std::size_t height>
+class ExampleMap
+{
+public:
+    bool is_open(int x, int y) const
+    {
+        return x > 0 && y > 0 && x < width && y < height && !walls_[y][x];
+    }
+
+    void set_wall(int x, int y, bool wall)
+    {
+        walls_[y][x] = wall;
+    }
+
+private:
+    std::array<std::array<bool, width>, height> walls_{};
+};
+
+template <typename MapType>
+class ExamplePathNode
+{
+public:
+    using Map = MapType;
+
+    // pair(cost, adjacent_node)
+    using MoveList = std::vector<std::pair<int,ExamplePathNode>>;
+
+    ExamplePathNode(Map *p_map = nullptr, int x = 0, int y = 0)
+        : p_map_(p_map)
+        , x_(x)
+        , y_(y)
+    { }
+
+    ExamplePathNode(const ExamplePathNode& src) = default;
+    ExamplePathNode& operator=(const ExamplePathNode& src) = default;
+
+    int get_x() const
+    {
+        return x_;
+    }
+
+    int get_y() const
+    {
+        return y_;
+    }
+
+    MoveList get_moves() const
+    {
+        MoveList move_list;
+        for(auto& location : std::array<std::pair<int, int>, 4> {{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}})
+        {
+            if(p_map_->is_open(x_ + location.first, y_ + location.second))
+            {
+                move_list.push_back(std::pair<int, ExamplePathNode>(1, {p_map_, x_ + location.first, y_ + location.second}));
+            }
+        }
+        return move_list;
+    }
+
+    bool operator==(const ExamplePathNode& rhs) const
+    {
+        return p_map_ == rhs.p_map_ && x_ == rhs.x_ && y_ == rhs.y_;
+    }
+
+    bool operator!=(const ExamplePathNode& rhs) const
+    {
+        return !(*this == rhs);
+    }
+
+private:
+    Map *p_map_;
+    int x_;
+    int y_;
+};
+
+namespace std
+{
+
+template <typename Map>
+struct hash<ExamplePathNode<Map>>
+{
+    using argument_type = ExamplePathNode<Map>;
+    using result_type = std::size_t;
+
+    result_type operator()(const argument_type& arg) const
+    {
+        return (arg.get_x() << 16 | arg.get_x() >> 16) ^ arg.get_y();
+    }
+};
+
+}
+
+template <typename Map>
+struct ExampleHeuristic
+{
+    int operator()(const ExamplePathNode<Map>& lhs, const ExamplePathNode<Map>& rhs)
+    {
+        // manhattan distance
+        return std::abs(lhs.get_x() - rhs.get_x()) + std::abs(lhs.get_y() - rhs.get_y());
+    }
+};
+
+
+template <typename Path>
+//static std::ostream& print_path(std::ostream& out, const decltype(std::declval<PathFinder<ExamplePathNode<Map>, ExampleHeuristic<Map>>>().get_optimal_path(std::declval<ExamplePathNode<Map>>()))& path)
+static std::ostream& print_path(std::ostream& out, const Path& path)
+{
+    if(path == boost::none)
+    {
+        std::cout << "none";
+    }
+    else {
+        for(auto& node : path.get())
+        {
+            std::cout << " -> {" << node.get_x() << ", " << node.get_y() << "}";
+        }
+    }
+
+    return out;
+}
+
+
+int main()
+{
+    using Map = ExampleMap<5, 5>;
+    Map map;
+    
+    map.set_wall(1, 1, true);
+    map.set_wall(1, 2, true);
+    map.set_wall(1, 3, true);
+
+    map.set_wall(2, 0, true);
+    map.set_wall(2, 1, true);
+    map.set_wall(2, 3, true);
+
+    map.set_wall(3, 1, true);
+    map.set_wall(3, 2, true);
+    map.set_wall(3, 3, true);
+
+    PathFinder<ExamplePathNode<Map>, ExampleHeuristic<Map>> path_finder(ExamplePathNode<Map>(&map, 0, 0));
+
+    std::cout << "path to middle: ";
+    print_path(std::cout, path_finder.get_optimal_path(ExamplePathNode<Map>(&map, 2, 2)));
+    std::cout << std::endl;
+
+    std::cout << "path to upper-right: ";
+    print_path(std::cout, path_finder.get_optimal_path(ExamplePathNode<Map>(&map, 0, 3)));
+    std::cout << std::endl;
+    //std::cout << "path to middle: " << path_finder.get_optimal_path(ExamplePathNode<Map>(&map, 2, 2)) << std::endl;
+    //std::cout << "path to upper-right: " << path_finder.get_optimal_path(ExamplePathNode<Map>(&map, 0, 3)) << std::endl;
+}
--- GameEngineProject/GameEngineProject/PathFinder.h
+++ GameEngineProject/GameEngineProject/PathFinder.h
@@ -1,103 +1,14 @@
 #pragma once
 
-template <std::size_t Width, std::size_t Height>
-class ExampleMap
-{
-public:
-    constexpr auto width = Width;
-    constexpr auto height = Height;
+#include <boost/optional.hpp>
 
-    bool is_open(int x, int y) const
-    {
-        return x > 0 && y > 0 && x < width && y < height && !walls_[y][x];
-    }
-
-    void set_wall(int x, int y, bool wall)
-    {
-        walls_[y][x] = wall;
-    }
-
-private:
-    std::array<std::array<bool, width> height> walls_{};
-};
-
-class ExamplePathNode
-{
-public:
-
-    // pair(cost, adjacent_node)
-    using MoveList = std::vector<std::pair<int,ExamplePathNode>>;
-
-    ExamplePathNode(ExampleMap *p_map = nullptr, int x = 0, int y = 0)
-        : p_map_(p_map)
-        , x_(x)
-        , y_(y)
-    { }
-
-    ExamplePathNode(const ExamplePathNode& src) = default;
-    ExamplePathNode& operator=(const ExamplePathNode& src) = default;
-
-    int get_x() const
-    {
-        return x_;
-    }
-
-    int get_y() const
-    {
-        return y_;
-    }
-
-    NodeList get_moves() const
-    {
-        NodeList node_list;
-        for(auto& location : std::array<std::pair<int>> {{0, -1}, {0, 1}, {-1, 0}, {1, 0}})
-        {
-            if(p_map_->is_open(x_ + location.first, y_ + location.second))
-            {
-                node_list.emplace_back(1, {p_map_, x_ + location.first, y_ + location.second});
-            }
-        }
-        return node_list;
-    }
-
-    bool operator==(const ExamplePathNode& rhs) const
-    {
-        return p_map_ == rhs.p_map_ && x_ == rhs.x_ && y_ == rhs.y_;
-    }
-
-private:
-    ExampleMap *p_map_;
-    int x_;
-    int y_;
-};
-
-namespace std
-{
-
-template <>
-struct hash<ExamplePathNode>
-{
-    using argument_type = ExamplePathNode;
-    using result_type = std::size_t;
-
-    result_type operator()(const argument_type& arg) const
-    {
-        return (x_ << 16 | x_ >> 16) ^ y_;
-    }
-};
-
-}
-
-
-struct ExampleHeuristic
-{
-    int operator()(const ExamplePathNode& lhs, const ExamplePathNode& rhs)
-    {
-        // manhattan distance
-        return std::abs(lhs.get_x() - rhs.get_x()) + std::abs(lhs.get_y(), rhs.get_y());
-    }
-};
-
+#include <algorithm>
+#include <array>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 template <typename PathNodeType, typename HeuristicType>
 class PathFinder
@@ -107,7 +18,7 @@ public:
     using Heuristic = HeuristicType;
 
     using Path = std::vector<PathNode>;
-    using Cost = decltype(heuristic_(start_node_, start_node_));
+    using Cost = decltype(std::declval<Heuristic>()(std::declval<PathNode>(), std::declval<PathNode>()));
 
     PathFinder(PathNode start_node, Heuristic heuristic = Heuristic());
 
@@ -115,7 +26,7 @@ public:
 
 private:
     PathNode start_node_;
-    Heurstic heuristic_;
+    Heuristic heuristic_;
     std::unordered_map<PathNode, PathNode> optimal_previous_nodes_;
     std::unordered_map<PathNode, Cost> g_costs_;
     std::unordered_set<PathNode> unexplored_nodes_;
@@ -125,8 +36,8 @@ private:
 };
 
 
-template <typename ...T>
-PathFinder<T...>::get_optimal_path(PathNode start_node, Heuristic heuristic)
+template <typename PathNodeType, typename HeuristicType>
+PathFinder<PathNodeType, HeuristicType>::PathFinder(PathNode start_node, Heuristic heuristic)
     : start_node_(start_node)
     , heuristic_(heuristic)
 {
@@ -135,8 +46,8 @@ PathFinder<T...>::get_optimal_path(PathNode start_node, Heuristic heuristic)
     optimal_previous_nodes_[start_node_] = start_node_;
 }
 
-template <typename ...T>
-auto PathFinder<T...>::get_optimal_path(PathNode end_node) -> boost::optional<Path>
+template <typename PathNodeType, typename HeuristicType>
+auto PathFinder<PathNodeType, HeuristicType>::get_optimal_path(PathNode end_node) -> boost::optional<Path>
 {
     // while path not found
     while(optimal_previous_nodes_.count(end_node) == 0)
@@ -145,13 +56,13 @@ auto PathFinder<T...>::get_optimal_path(PathNode end_node) -> boost::optional<Pa
         {
             return boost::none;
         }
-        auto calc_f_cost = [=this, =end_node] (const PathNode& unexplored)
+        auto calc_f_cost = [this, end_node] (const PathNode& unexplored)
         {
             return g_costs_[unexplored] + heuristic_(unexplored, end_node);
         };
 
-        auto to_explore = std::min_element(unexplored_nodes_.begin(), unexplored_nodes_.end(),
-            [=this, =end_node] (const PathNode& lhs, const PathNode& rhs)
+        auto to_explore = *std::min_element(unexplored_nodes_.begin(), unexplored_nodes_.end(),
+            [this, end_node, &calc_f_cost] (const PathNode& lhs, const PathNode& rhs)
             {
                 return calc_f_cost(lhs) < calc_f_cost(rhs);
             }
@@ -176,4 +87,16 @@ auto PathFinder<T...>::get_optimal_path(PathNode end_node) -> boost::optional<Pa
         }
     }
     return reconstruct_path_(end_node);
+};
+
+template <typename PathNodeType, typename HeuristicType>
+auto PathFinder<PathNodeType, HeuristicType>::reconstruct_path_(PathNode end_node) const -> Path
+{
+    Path path;
+    for(PathNode walk_node = end_node; walk_node != start_node_; walk_node = optimal_previous_nodes_.at(walk_node))
+    {
+        path.push_back(end_node);
+    }
+    std::reverse(path.begin(), path.end());
+    return path;
 };
