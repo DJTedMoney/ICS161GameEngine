7150284f34d0d59e120bfbf8f518dd023ee97bb2
Hector Martinez <hectoram@uci.edu>
2015-02-24 16:48:44 -0800

    Can now open and write simple strings to files.

--- GameEngineProject/GameEngineProject/SaveHandler.h
+++ GameEngineProject/GameEngineProject/SaveHandler.h
@@ -8,14 +8,17 @@ class SaveHandler
 
 public:
 	//Might need to have a string as a parameter to know where to look to load sounds
-
+	const int TOTAL_DATA = 10;
 	static SaveHandler* getInstance();
+
+	//"C:\\Users\\Hector\\Desktop\\161\\FileToLoad\\"
+	std::string test = "C:\\Users\\Hector\\Desktop\\161\\FileToLoad\\sample.txt";
 
 
 	bool save();
 	bool load();
 
-	bool write();
+	bool write(std::string toWrite);
 	bool delte();
 	bool open(std::string path);
 
@@ -24,17 +27,12 @@ public:
 	bool errorExists;
 
 	int gData[10];
-
-private:
-
-	SDL_RWops* file;
 	std::string path;
-	std::queue<std::string>  eventQueue;
 
-	 static SaveHandler* instance;
-	
+private:	
 	 //Singleton class to avoid writing to same file multiple times.
 	 SaveHandler(const SaveHandler&);
 	 SaveHandler& operator=(const SaveHandler&);
-	 SaveHandler() {}
+	 SaveHandler() {};
+	 static SaveHandler* instance;
 };
--- GameEngineProject/GameEngineProject/SaveHandlercpp.cpp
+++ GameEngineProject/GameEngineProject/SaveHandlercpp.cpp
@@ -14,9 +14,10 @@ SaveHandler* SaveHandler::getInstance()
 	return instance;
 }
 
+//Checks to see if the file can be opened.
 bool SaveHandler::open(std::string pathToFile)
 {
-	file = SDL_RWFromFile(pathToFile.c_str(), "r+b");
+	SDL_RWops* file = SDL_RWFromFile(test.c_str(), "r+");
 
 	if (file == NULL)
 		std::cout << "Warning: Unable to open file! SDL Error: " << SDL_GetError() << std::endl;
@@ -24,7 +25,7 @@ bool SaveHandler::open(std::string pathToFile)
 	if (file != NULL)
 	{
 		std::cout << "New file created!" << std::endl;
-		for (int i = 0; i < 10; ++i)
+		for (int i = 0; i < TOTAL_DATA; ++i)
 		{
 			gData[i] = 0;
 			SDL_RWwrite(file, &gData[i], sizeof(Sint32), 1);
@@ -37,25 +38,42 @@ bool SaveHandler::open(std::string pathToFile)
 		std::cout << "Error: Unable to create file! SDL Error: " << SDL_GetError() << std::endl;
 		errorExists = true;
 	}
+	
+	return true;
 }
 
 
 bool SaveHandler::load()
 {
-
+	return true;
 }
 
 bool SaveHandler::save()
 {
-
+	return true;
 }
 
 bool SaveHandler::delte()
 {
-
+	return true;
 }
-
-bool SaveHandler::write()
+//Writes string to a file. Doesn't take spaces yet. 
+bool SaveHandler::write(std::string toWrite)
 {
+	SDL_RWops *file = SDL_RWFromFile(test.c_str(), "w");
 
+	if (file != NULL) 
+	{
+		size_t len = SDL_strlen(toWrite.c_str());
+
+		if (SDL_RWwrite(file, toWrite.c_str(), 1, len) != len) {
+			std::cout << "Could not write string!" << std::endl;
+		}
+		else {
+			//printf("Wrote %d 1-byte blocks\n", len);
+		}
+		SDL_RWclose(file);
+	}
+
+	return true;
 }
