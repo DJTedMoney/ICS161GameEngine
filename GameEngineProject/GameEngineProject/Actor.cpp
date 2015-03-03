651acb6ba0a3b270f3afffa687e5917558246d7e
Raymond Chen <raymoc2@uci.edu>
2015-03-02 22:51:05 -0800

    Edits to Actor.h and Actor.cpp

---
+++ GameEngineProject/GameEngineProject/Actor.cpp
@@ -0,0 +1,57 @@
+#include "Actor.h"
+
+Actor::Actor(SpriteHandler* sprite, bool isInteractable, int x, int y)
+:sprite(sprite), interactable(isInteractable), x(x), y(y) {}
+
+Actor::~Actor() {
+}
+
+void Actor::setSprite(SpriteHandler* new_sprite) {
+	sprite = new_sprite;
+}
+
+void Actor::moveAbsolute(int new_x, int new_y) {
+	x = new_x;
+	y = new_y;
+}
+
+void Actor::moveRelative(int deltaX, int deltaY) {
+	x += deltaX;
+	y += deltaY;
+}
+
+bool Actor::isInteractable() {
+	return interactable;
+}
+
+void Actor::toggleInteractable() {
+	interactable = !interactable;
+}
+
+std::string Actor::getName() {
+	return name;
+}
+
+void Actor::setName(std::string new_name) {
+	name = new_name;
+}
+
+void Actor::addAttribute(std::string key, std::string value) {
+	attributes[key] = value;
+}
+
+std::string Actor::getAttribute(std::string key) {
+	return attributes[key];
+}
+
+void Actor::removeAttribute(std::string key) {
+	attributes.erase(key);
+}
+
+void Actor::executeAI(void(*func)()) {
+
+}
+
+void Actor::detectCollision() {
+
+}
--- GameEngineProject/GameEngineProject/Actor.h
+++ GameEngineProject/GameEngineProject/Actor.h
@@ -5,33 +5,42 @@
 
 class Actor {
 public:
-	Actor(SpriteHandler sprite, bool isInteractable, int x, int y);
+	Actor(SpriteHandler* sprite, bool isInteractable, int x, int y);
 	~Actor();
 
-	void setSprite(SpriteHandler sprite);
+	// Sprite
+	void setSprite(SpriteHandler* new_sprite);
 
 	// Positioning
-	void moveAbsolute(int x, int y);
+	void moveAbsolute(int new_x, int new_y);
 	void moveRelative(int deltaX, int deltaY);
 
+	// Interactability 
 	bool isInteractable(); // returns interactable var
 	void toggleInteractable(); // switch between interactable and not interactable
 
+	// Identifier
 	std::string getName();
-	void setName(std::string name);
+	void setName(std::string new_name);
 
 	// Attributes
 	void addAttribute(std::string key, std::string value);
 	std::string getAttribute(std::string key);
 	void removeAttribute(std::string key);
 
+	// AI
+	void executeAI( void (*func)() ); // takes function pointer
+
+	// Collision
+	void detectCollision();
+
 private:
-	SpriteHandler sprite; 
+	SpriteHandler* sprite; 
 
 	std::map<std::string, std::string> attributes; //key-value pairs describing attributes of the actor
 
 	int x;
 	int y;
 	std::string name; // identifier
-	bool isInteractable; // can we interact with this object/entity?
+	bool interactable; // can we interact with this object/entity?
 };
