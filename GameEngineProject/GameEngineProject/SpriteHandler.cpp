53574e6875c89d68538fe19924e8b297079f41d5
Peeka13 <shohmann@uci.edu>
2015-03-10 17:39:25 -0700

    Implemented SpriteHandler.cpp

--- GameEngineProject/GameEngineProject/SpriteHandler.cpp
+++ GameEngineProject/GameEngineProject/SpriteHandler.cpp
@@ -5,4 +5,27 @@ SpriteHandler::SpriteHandler()
 
 }
 
-void makeSprite()
+void SpriteHandler::makeSprite(Sprite s)
+{
+	sprites.push_back(s);
+}
+
+void SpriteHandler::displaySprite(int index, int fIndex)
+{
+	sprites[index].show(fIndex);
+}
+
+void SpriteHandler::displaySprite(int index, std::string seq)
+{
+	sprites[index].show(seq);
+}
+
+Sprite SpriteHandler::getSprite(int index)
+{
+	return sprites[index];
+}
+
+void SpriteHandler::checkCollision(int index)
+{
+	sprites[index].checkCollision();
+}
